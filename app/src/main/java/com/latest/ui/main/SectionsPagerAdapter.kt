522cb18f5ad87047dbd74cd323ea380883127bd0
bhavinv821 <bhavinv821@gmail.com>
2020-06-08 10:45:58 -0400

    updating

--- app/src/main/java/com/latest/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/latest/ui/main/SectionsPagerAdapter.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.latest.R
+import com.latest.*
 
 private val TAB_TITLES = arrayOf(
         R.string.tab_text_1,
@@ -24,6 +24,29 @@ class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
 
 
     override fun getItem(position: Int): Fragment {
+
+    when(position){
+        0->{
+            val app = appFragment()
+            return app
+        }
+        1-> {
+            val photo = photoFragment()
+            return photo
+        }
+        2->{
+            val video = videoFragment()
+            return video
+        }
+        3->{
+            val music  = musicFragment()
+            return music
+        }
+        4->{
+            val file = fileFragment()
+            return file;
+        }
+    }
 
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
